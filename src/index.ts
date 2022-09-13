35b400b898273791119913680c20c6472466148e
devliss <liseyna1@gmail.com>
2022-09-13 20:26:00 +0300

    Реализованы методы API без валидации

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 // @ts-ignore
 import express,{Request, Response} from 'express'
 import {data} from "./data"
+import {videoType} from "./types";
+import bodyParser from "body-parser";
 const app = express()
 const port = process.env.PORT || 5000
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware);
 
 app.get('/',(req:Request, res:Response)=>{
     res.send('Hello!')
@@ -11,7 +15,6 @@ app.get('/',(req:Request, res:Response)=>{
 app.get('/hometask_01/api/videos',(req:Request, res:Response)=>{
     res.send(data)
 })
-
 app.get('/hometask_01/api/videos/:id',(req:Request, res:Response)=>{
 
     let video = data.find(v => v.id === +req.params.id)
@@ -20,7 +23,6 @@ app.get('/hometask_01/api/videos/:id',(req:Request, res:Response)=>{
     }
     res.send(video)
 })
-
 app.delete('/hometask_01/api/videos/:id',(req:Request, res:Response)=>{
     for(let i=0; i < data.length; i++){
         if(data[i].id === +req.params.id) {
@@ -30,6 +32,36 @@ app.delete('/hometask_01/api/videos/:id',(req:Request, res:Response)=>{
         }
     }
     res.send(404)
+})
+app.put('/hometask_01/api/videos/:id',(req:Request, res:Response)=>{
+    const video = data.find(v => v.id === +req.params.id)
+    if(!video)
+        res.send(404)
+    else{
+            video.title = req.body.title
+            video.author = req.body.author
+            video.availableResolutions= req.body.availableResolutions
+            video.canBeDownloaded = req.body.canBeDownloaded
+            video.minAgeRestriction = req.body.minAgeRestriction
+            video.publicationDate = req.body.publicationDate
+
+        res.send(204)
+    }
+})
+app.post('/hometask_01/api/videos',(req:Request, res:Response)=>{
+    const video:videoType = {
+        id:+(new Date()),
+        title: req.body.title,
+        author: req.body.author,
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date().toISOString(),
+        availableResolutions: req.body.availableResolutions
+    }
+
+    data.push(video);
+    res.status(201).send(video);
 })
 app.listen(port,()=>{
     console.log(`Listening port ${port}`);
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface videoType{
+    "id": number,
+    "title": string,
+    "author": string,
+    "canBeDownloaded": boolean,
+    "minAgeRestriction":null,
+    "createdAt": string,
+    "publicationDate": string,
+    "availableResolutions": string[]
+}
